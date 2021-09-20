e8ec0223e92272ea58b2f22a12cb05a013de4c13
Helen Soonets <Helen@Helens-MacBook-Pro.local>
2021-09-21 00:02:47 +0300

    added task5.js function checkstring

---
+++ task5.js
@@ -0,0 +1,25 @@
+let str = "";
+function checkstring(str) {
+    if(str) {
+        return true;
+    } else if(str=== ""){
+        return false;
+    }else{
+        return false;
+    }
+}
+
+function uppercase(str) {
+    if(str){
+        return str.charAt (0).toUpperCase() + str.slice(1);
+    }else{
+        return '""';
+    }
+}
+
+console.log(checkstring());
+console.log(checkstring("tere"));
+console.log(checkstring ("minu nimi on John"));
+console.log(uppercase());
+console.log(uppercase("tere"));
+console.log (uppercase("minu nimi on John"));
