e89be011d97387a841a4caaf5356c9b20acb3851
Helen Soonets <Helen@Helens-MacBook-Pro.local>
2021-09-28 19:10:29 +0300

    Added task6.js

---
+++ task6.js
@@ -0,0 +1,20 @@
+function hello(n) {
+    console.log('n = ' + n)
+        if (!n) {
+            console.log('Hi!')
+        } else if (n > 10) {
+            for( let i=0; i<n; i++ ) {
+                console.log('Hi!')
+            }
+        } else {
+            for( let i=0; i<n; i++ ) {
+                console.log('Hello!')
+        }
+    }
+}
+
+
+hello()
+hello(2)
+hello(3)
+hello(12)
---
+++ task8.js
@@ -0,0 +1,8 @@
+const moviedata = require('./moviedata.json')
+//console.log('moviedata', moviedata)
+
+//Ex 1
+const year = "Year"
+typeof year
+const yearAsNumber = Number(year)
+typeof yearAsNumber
