43e644651de83371772d05fd7d0201387ba22f14
Helen Soonets <Helen@Helens-MacBook-Pro.local>
2021-09-29 21:06:12 +0300

    Added task8.js

--- task8.js
+++ task8.js
@@ -2,7 +2,21 @@ const moviedata = require('./moviedata.json')
 //console.log('moviedata', moviedata)
 
 //Ex 1
-const year = "Year"
-typeof year
-const yearAsNumber = Number(year)
-typeof yearAsNumber
+
+//Ex 2
+const movieDrama = moviedata.filter((p) => {
+  if (p.Genre.includes("Drama")) {
+      return p
+  }
+})
+//console.log('Drama', movieDrama)
+
+//Ex 3
+function ratedR (Rated) {
+    return moviedata.filter((movie) => {
+      return movie.Rated === Rated
+    })
+  }
+  console.log(ratedR("R"))
+
+
